3c4172617997696198df283ad6fa0f0d2289ffa6
Nox <juan.angarita.11@gmail.com>
2019-04-28 06:12:57 -0500

    Final cleaning

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="author" content="Anindya Dhruba">
-        <meta name="description" content="Laravel Vue SPA">
+        <meta name="author" content="Rigel">
+        <meta name="description" content="Antares">
 
-        <title>{{ config('app.name') }} - Starter Boilerplate SPA made with Laravel 5.6, Bootstrap 4, JWT Auth, Vue 2, Vue-router 3, Vuex 3, Axios and <3</title>
+        <title>{{ config('app.name') }}</title>
 
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
         <script>
