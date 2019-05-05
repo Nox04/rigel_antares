a40ce8a5f262b94030d83e75884369d2ebc21769
Nox <juan.angarita.11@gmail.com>
2019-05-04 23:41:27 -0500

    Fix input

--- resources/js/components/controls/form/TextInput.vue
+++ resources/js/components/controls/form/TextInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="form-group">
     <label class="capitalized">{{label}} <span v-show="required">*</span></label>
-    <input @keyup="validate" :type="type === 'pin' ? 'password': type" v-model="value" class="form-control" :placeholder="'Ingrese el ' + label">
+    <input @change="validate" :type="type === 'pin' ? 'password': type" v-model="value" class="form-control" :placeholder="`Ingrese el ${label}`">
     <span v-show="error" class="form-text text-danger">{{errorMessage}}</span>
   </div>
 </template>
